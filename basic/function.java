70d766aeda80de8c6dc8e8b02fa1855ed2210be1
Jenis <jeniswinsta@gmail.com>
2025-03-20 07:41:13 +0530

    commit 1 20/3

--- basic/function.java
+++ basic/function.java
@@ -38,14 +38,47 @@ public class function {
     }
 
     public static void decimalToBinary(int n){
-        
+        int binary=0;
+        int pow=0;
+        // int last{digit=0;
+        while(n>0){
+            // System.out.print("binary before: "+binary);
+            binary= binary +((n%2)*(int)Math.pow(10,pow));
+            n=n/2;
+            pow++;
+        }
+        System.out.print(binary);
     }
+
+    public static int reverse(int n){
+        int reverse=0;
+        while(n>0){
+            reverse=reverse*10+(n%10);
+            n=n/10;
+        }
+        System.out.println(reverse);
+        return reverse;
+    }
+
+    public static void palindrome(int n){
+        int reverse=reverse(n);
+        if(n==reverse){
+            System.out.println(n+"is a Palindrome!!");
+        }
+        else{
+            System.out.println(n+" is Not a palindrome");
+        }
+
+    }
+
+
 
     public static void main(String[] args) {
 
         // prime_range(20);
-        binaryToDecimal(1010);
-        decimalToBinary(10);
+        // binaryToDecimal(1010);
+        // decimalToBinary(2);
+        palindrome(121);
 
     }
 }
